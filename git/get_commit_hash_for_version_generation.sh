6c96a5c6d447e90bc79c9d63151033a891913545
realitycheck <realitycheck@users.noreply.github.com>
2023-06-23 15:44:29 +0300

    Create get_commit_hash_for_version_generation.sh

---
+++ git/get_commit_hash_for_version_generation.sh
@@ -0,0 +1,8 @@
+# long hash: 94cd6ca67c801c8b41f0b103be0b19906b411d00
+git log -1 --format=%H
+
+# short hash: 9411d00
+git rev-parse --short HEAD  
+
+# nearest tag, commit number, short commit hash: 1.0.0-1123-g9411d00
+shell git describe --tags --always 
